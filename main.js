371b9337d9eb41cd7c5136bb23356a022a20a888
xtha1 <shankarxtha@gmail.com>
2020-03-30 12:23:17 +0545

    adding tota deaths and other

--- main.js
+++ main.js
@@ -1,21 +1,26 @@
 // let who='';
-
 let URL='https://nepalcorona.info/api/v1/data/world'
 fetch(URL)
 .then(function(response){
    return response.json()
 }).then(function(covid){
 //    console.log(covid);
-// who=covid;
-   covid.forEach(function(data)
-   {
-    let virus=document.createElement('div');
-    virus.innerHTML=data.country; 
-    document.body.appendChild(virus);
-
-       let cases=document.createElement('div');
-       cases.innerHTML=data.totalCases;
-       document.body.appendChild(cases);
+       if(covid.length>0)
+       {
+           var coviddata='';
+           covid.forEach(function(data){
+            // coviddata +="<tr>";
+            coviddata +="<tr><td>"+data.country+"</td>";
+            coviddata +="<td>"+data.totalCases+"</td>";
+            coviddata +="<td>"+data.newCases+"</td>";
+            coviddata +="<td>"+data.totalDeaths+"</td>";
+            coviddata +="<td>"+data.newDeaths+"</td>";
+            coviddata +="<td>"+data.activeCases+"</td>";
+            coviddata +="<td>"+data.totalRecovered+"</td>";
+            coviddata +="<td>"+data.criticalCases+"</td></tr>";
+            console.log(coviddata);
+            document.getElementById('coranavirus').innerHTML=coviddata;
+           })
+       }
    })
-        
-});
+
